fbb3ffd137ec0500432d192fd9cd6a7ccca8a45f
okinashunsuke <kiseven0734@gmail.com>
2020-11-13 19:13:42 +0900

    商品出品機能実装

--- Gemfile
+++ Gemfile
@@ -54,17 +54,18 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+
+
 group :development do
   gem 'rubocop', require: false
 end
-
 group :production do
   gem 'rails_12factor'
 end
-
 gem 'devise'
 gem 'pry-rails'
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 gem 'factory_bot_rails'
 gem 'faker'
+gem 'active_hash'
---
+++ app/models/item.rb
@@ -0,0 +1,36 @@
+class Item < ApplicationRecord
+  
+  with_options presence: true ,numericality: { other_than: 1 }do  
+   validates :category_id     
+   validates :condition_id    
+   validates :postage_id      
+   validates :prefecture_id   
+   validates :handing_time_id 
+  end
+
+  with_options presence: true do
+   validates :image
+   validates :name        
+   validates :description 
+   validates :price       , format: { with: /\A[0-9]+\z/} , inclusion: {in: 300..9999999 }  
+  end
+  
+  # def was_attached?
+  #   self.image.attached?
+  # end
+ 
+  has_one    :purchase
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :postage
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :handing_time
+end

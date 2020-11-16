c8adc324fd69bf9f291d66e763f104fe750d0de3
okinashunsuke <kiseven0734@gmail.com>
2020-11-16 12:24:46 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,]
+  before_action :authenticate_user!, except: [:index,:show]
 
 def index  
   @items = Item.order("created_at DESC")
@@ -12,6 +12,20 @@ end
 # end
 
 def show
+  @item = Item.find(params[:id]) 
+  @name = @item.name
+  @description = @item.description
+  @price = @item.price
+  @category = @item.category[:name]
+  @condition = @item.condition[:name]
+  @postage = @item.postage[:name]
+  @prefecture = @item.prefecture[:name]
+  @handing_time  = @item.handing_time[:name]
+  @image = @item.image
+  
+  user_id = @item.user_id
+  @user = User.find(user_id)
+  @nickname = @user.nickname
 end
 
 

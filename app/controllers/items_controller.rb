7a5c927f773823f33d672fd5703269f71210bc5b
okinashunsuke <kiseven0734@gmail.com>
2020-11-16 18:13:20 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-
+  before_action :move_to_index, only: [:edit]
 def index  
   @items = Item.order("created_at DESC")
 
@@ -15,6 +15,18 @@ def show
   @item = Item.find(params[:id]) 
 end
 
+def edit
+  @item = Item.find(params[:id]) 
+end
+
+def update
+  @item = Item.find(params[:id]) 
+  if @item.update(item_params)
+    redirect_to action: :show
+  else
+    render :edit  
+  end
+end
 
 
 def new 
@@ -37,4 +49,14 @@ end
    params.require(:item).permit(:image, :name, :description, :price, :category_id, :condition_id, :postage_id, :prefecture_id, :handing_time_id ).merge(user_id: current_user.id)
   end
   
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+
+
+
  end

9b7c231886d046700c7cd5b753ab8469d9136ed9
okinashunsuke <kiseven0734@gmail.com>
2020-11-16 12:59:16 +0900

    詳細表示修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,19 +13,8 @@ end
 
 def show
   @item = Item.find(params[:id]) 
-  @name = @item.name
-  @description = @item.description
-  @price = @item.price
-  @category = @item.category[:name]
-  @condition = @item.condition[:name]
-  @postage = @item.postage[:name]
-  @prefecture = @item.prefecture[:name]
-  @handing_time  = @item.handing_time[:name]
-  @image = @item.image
-  
   user_id = @item.user_id
   @user = User.find(user_id)
-  @nickname = @user.nickname
 end
 
 

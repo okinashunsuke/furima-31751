0ec4ff11065f7ed0b756a5bc5344eeb98ce12a1c
okinashunsuke <kiseven0734@gmail.com>
2020-11-16 19:36:23 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:edit,:destroy]
   
   
 def index  
@@ -9,10 +9,13 @@ def index
 
 end
 
-# def destroy
-#   @item = Item.all
-#   @item.destroy
-# end
+def destroy
+  if  @item.destroy
+    redirect_to action: :index
+  else
+    render :show  
+  end
+end
 
 def show
 end
